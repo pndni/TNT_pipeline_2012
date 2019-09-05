ba4b67e54906b891f46eeabacf140d7129241536
Steven Tilley <stilley@hollandbloorview.ca>
2019-09-05 10:39:06 -0400

    add download software command

---
+++ download_software.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+
+wget https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.6/hdf5-1.6.10/src/hdf5-1.6.10.tar.gz
+wget ftp://ftp.unidata.ucar.edu/pub/netcdf/old/netcdf-4.0.1.tar.gz
+wget https://packages.bic.mni.mcgill.ca/tgz/minc-2.0.18.tar.gz
+wget https://packages.bic.mni.mcgill.ca/tgz/ebtks-1.6.3.tar.gz
+wget https://packages.bic.mni.mcgill.ca/tgz/mni_perllib-0.08.tar.gz
+wget https://packages.bic.mni.mcgill.ca/tgz/N3-1.11.0.tar.gz
+wget https://packages.bic.mni.mcgill.ca/tgz/inormalize-1.0.2.tar.gz
+wget https://packages.bic.mni.mcgill.ca/tgz/classify-1.0.08.tar.gz
+wget https://fsl.fmrib.ox.ac.uk/fsldownloads/oldversions/fsl-4.1.9-sources.tar.gz
+wget https://cmake.org/files/v2.8/cmake-2.8.12.2-Linux-i386.tar.gz
+wget https://sourceforge.net/projects/itk/files/itk/3.20/InsightToolkit-3.20.0.tar.gz
+wget http://www.bic.mni.mcgill.ca/~vfonov/software/ezminc-r60.tar.gz
+wget http://www.bic.mni.mcgill.ca/~vfonov/software/mincANTS_1p9.tar.gz
+wget ftp://ftp.gnu.org/gnu/gsl/gsl-1.14.tar.gz
