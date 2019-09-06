98b26b47c1f6b4fdaea90449a97babb08fbf4acd
Steven Tilley <stilley@hollandbloorview.ca>
2019-09-06 12:44:11 -0400

    update to allow generic commits

--- build_and_upload_docker.sh
+++ build_and_upload_docker.sh
@@ -6,9 +6,13 @@ set -u
 ver=$1
 tmpdir=$(mktemp -d)
 
-git clone --branch $ver git@github.com:pndni/TNT_pipeline_2012.git $tmpdir
+git clone git@github.com:pndni/TNT_pipeline_2012.git $tmpdir
 pushd $tmpdir
 
+git checkout $ver
 ./download_software.sh
 docker build -t pndni/tnt_pipeline_2012:$ver .
 docker push pndni/tnt_pipeline_2012:$ver
+
+popd
+rm -rf $tmpdir
